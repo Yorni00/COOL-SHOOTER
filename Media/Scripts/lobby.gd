04ba9d932acc7cfc1d753b7b431680e124b3189e
Zeller <example@gmail.com>
2024-06-04 15:16:24 +0700

    0.0.1.5

--- Media/Scripts/lobby.gd
+++ Media/Scripts/lobby.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+@onready var SV = $Selector
 
 func _on_back_b_pressed():
 	get_tree().change_scene_to_file("res://Media/Scenes/menu.tscn")
+
+func _ready():
+	print(TheHeader.numbers)
+
+func _input(event):
+	pass
+
+
+func _on_selector_b_pressed():
+	if SV.visible == true:
+		SV.visible = false
+	if SV.visible == false:
+		SV.visible == true
+	print(SV.visible)
---
+++ Media/Scripts/the_header.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@export var numbers = [1,3,5,7,9]
